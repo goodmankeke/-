27883cd57dbbd64e2fca8e2d8027531a8266faaa
Chenkeke <502244366@qq.com>
2019-09-02 00:10:46 +0800

    测试

--- styles_ceshi.py
+++ styles_ceshi.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-ss = [(['名次'], ['学校名称'], ['星级排名'], ['办学层次'], ['总分']), (['名次'], ['学校名称'], ['星级排名'], ['办学层次'], ['总分'])]
-for i in ss:
-    u_list = []
-    for j in i:
-        ss = ''.join(j)
-        u_list.append(ss)
-    print(u_list)
-    # print(ss)
-    # print(u_list)
+# ss = [(['名次'], ['学校名称'], ['星级排名'], ['办学层次'], ['总分']), (['名次'], ['学校名称'], ['星级排名'], ['办学层次'], ['总分'])]
+# for i in ss:
+#     u_list = []
+#     for j in i:
+#         ss = ''.join(j)
+#         u_list.append(ss)
+#     print(u_list)
+# print(ss)
+# print(u_list)
+# '名次', '学校名称', '星级排名', '办学层次', '总分'
+fb = [['1', '北京大学', '8星级', '世界一流大学', '100']]
+
+for i in fb:
+    ss = {
+        "名次": i[0],
+        "学校名称": i[1],
+        "星级排名": i[2],
+        "办学层次": i[3],
+        "总分": i[4]
+    }
+
+    print(ss)
